490ca9121dbfd8b8a4a8ac1db2d47aceddf72626
Buvi <cje0173@naver.com>
2026-08-14 14:21:58 +0900

    feat: 안내프로그램.bat Edge 32/64비트 자동 경로 탐색 & 상대경로 탑재 (간결한 8줄 버전)

--- 안내프로그램.bat
+++ 안내프로그램.bat
@@ -1,4 +1,9 @@
 @echo off
 taskkill /F /IM msedge.exe /T >nul 2>&1
 timeout /t 2 >nul
-start "" "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --kiosk --force-device-scale-factor=1.75 "%~dp0index.html"
+
+set "EDGE=msedge.exe"
+if exist "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" set "EDGE=C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+if exist "C:\Program Files\Microsoft\Edge\Application\msedge.exe" set "EDGE=C:\Program Files\Microsoft\Edge\Application\msedge.exe"
+
+start "" "%EDGE%" --kiosk --force-device-scale-factor=1.75 "%~dp0index.html"
